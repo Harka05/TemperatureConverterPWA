712cdc201c4d2e1acb8c1ded2e9f0c6595281030
Harka05 <165388171+Harka05@users.noreply.github.com>
2025-10-30 14:34:41 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -34,47 +34,4 @@ self.addEventListener('fetch', event => {
     } 
   })()); 
 });
-const CACHE_NAME = 'temperature-converter-v1';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './converter.js',
-  './converter.css',
-  './manifest.json',
-  './icon512.png'
-];
-
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing...');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching app files...');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating...');
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('Deleting old cache:', key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
 
