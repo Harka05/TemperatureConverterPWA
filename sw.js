9b61c49bf5525fb65cb3019789e35016f07e7f56
Harka05 <165388171+Harka05@users.noreply.github.com>
2025-10-30 14:37:11 +0000

    Fix formatting issues in service worker code

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => { 
     const cache = await caches.open(CACHE_NAME); 
     cache.addAll([ 
-      './', 
-      './converter.js', 
-      './converter.css' 
+      '/', 
+      '/converter.js', 
+      '/converter.css' 
     ]); 
   })()); 
 }); 
@@ -34,4 +34,5 @@ self.addEventListener('fetch', event => {
     } 
   })()); 
 });
+
 
