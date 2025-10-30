9b7dbba6ff2ecccfc3d0d479f303bc5514a3a894
Harka05 <165388171+Harka05@users.noreply.github.com>
2025-10-30 14:31:56 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => { 
     const cache = await caches.open(CACHE_NAME); 
     cache.addAll([ 
-      '/', 
-      '/converter.js', 
-      '/converter.css' 
+      './', 
+      './converter.js', 
+      './converter.css' 
     ]); 
   })()); 
 }); 
@@ -77,3 +77,4 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
+
